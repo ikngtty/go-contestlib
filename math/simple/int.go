bed0db55314a78a04288e55cf28d3a7087bff3e8
ikngtty <ikngtty@gmail.com>
2020-07-11 17:34:34 +0900

    style: add an empty line after guard clauses

--- math/simple/int.go
+++ math/simple/int.go
@@ -13,6 +13,7 @@ func Min(values ...int) int {
 	if len(values) == 0 {
 		panic("no values")
 	}
+
 	min := values[0]
 	for _, v := range values {
 		if v < min {
@@ -26,6 +27,7 @@ func Max(values ...int) int {
 	if len(values) == 0 {
 		panic("no values")
 	}
+
 	max := values[0]
 	for _, v := range values {
 		if v > max {
@@ -39,6 +41,7 @@ func Pow(base, exponent int) int {
 	if exponent < 0 {
 		panic(fmt.Sprintf("exponent (%d) should not be a minus", exponent))
 	}
+
 	answer := 1
 	for i := 0; i < exponent; i++ {
 		answer *= base
