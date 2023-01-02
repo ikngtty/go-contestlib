d3234149cf1e71b9fff20ef8a6a1f95ef02829fb
ikngtty <ikngtty@gmail.com>
2023-01-02 19:08:52 +0900

    simplify `priorityqueue`
    Update may not be used.

--- collections/priorityqueue/int.go
+++ collections/priorityqueue/int.go
@@ -1,51 +1,19 @@
 package priorityqueue
 
-import "container/heap"
+type HeapInt []int
 
-// A PriorityQueueIntItem is something we manage in a priority queue.
-type PriorityQueueIntItem struct {
-	Value    int // The value of the item; arbitrary.
-	Priority int // The priority of the item in the queue.
-	// The index is needed by update and is maintained by the heap.Interface methods.
-	Index int // The index of the item in the heap.
+func (h HeapInt) Len() int           { return len(h) }
+func (h HeapInt) Less(i, j int) bool { return h[i] < h[j] }
+func (h HeapInt) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *HeapInt) Push(x interface{}) {
+	*h = append(*h, x.(int))
 }
 
-// A PriorityQueueInt implements heap.Interface and holds Items.
-type PriorityQueueInt []*PriorityQueueIntItem
-
-func (pq PriorityQueueInt) Len() int { return len(pq) }
-
-func (pq PriorityQueueInt) Less(i, j int) bool {
-	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
-	return pq[i].Priority > pq[j].Priority
-}
-
-func (pq PriorityQueueInt) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].Index = i
-	pq[j].Index = j
-}
-
-func (pq *PriorityQueueInt) Push(x interface{}) {
-	n := len(*pq)
-	item := x.(*PriorityQueueIntItem)
-	item.Index = n
-	*pq = append(*pq, item)
-}
-
-func (pq *PriorityQueueInt) Pop() interface{} {
-	old := *pq
+func (h *HeapInt) Pop() interface{} {
+	old := *h
 	n := len(old)
-	item := old[n-1]
-	old[n-1] = nil  // avoid memory leak
-	item.Index = -1 // for safety
-	*pq = old[0 : n-1]
-	return item
-}
-
-// Update modifies the priority and value of an Item in the queue.
-func (pq *PriorityQueueInt) Update(item *PriorityQueueIntItem, value int, priority int) {
-	item.Value = value
-	item.Priority = priority
-	heap.Fix(pq, item.Index)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
--- collections/priorityqueue/int_test.go
+++ collections/priorityqueue/int_test.go
@@ -7,65 +7,32 @@ import (
 )
 
 func TestInitAndPop(t *testing.T) {
-	pq := make(PriorityQueueInt, 5)
-	pq[0] = &PriorityQueueIntItem{Value: 0, Priority: 20, Index: 0}
-	pq[1] = &PriorityQueueIntItem{Value: 10, Priority: 40, Index: 1}
-	pq[2] = &PriorityQueueIntItem{Value: 20, Priority: 10, Index: 2}
-	pq[3] = &PriorityQueueIntItem{Value: 30, Priority: 50, Index: 3}
-	pq[4] = &PriorityQueueIntItem{Value: 40, Priority: 30, Index: 4}
+	pq := HeapInt{20, 40, 10, 50, 30}
 
 	heap.Init(&pq)
 
 	results := make([]int, 5)
 	for i := 0; i < 5; i++ {
-		results[i] = heap.Pop(&pq).(*PriorityQueueIntItem).Value
+		results[i] = heap.Pop(&pq).(int)
 	}
 
-	assertEqual(t, []int{30, 10, 40, 0, 20}, results)
+	assertEqual(t, []int{10, 20, 30, 40, 50}, results)
 }
 
 func TestPushAndPop(t *testing.T) {
-	pq := make(PriorityQueueInt, 3)
-	pq[0] = &PriorityQueueIntItem{Value: 0, Priority: 20, Index: 0}
-	pq[1] = &PriorityQueueIntItem{Value: 10, Priority: 40, Index: 1}
-	pq[2] = &PriorityQueueIntItem{Value: 20, Priority: 10, Index: 2}
+	pq := HeapInt{20, 40, 10}
 
 	heap.Init(&pq)
 
-	heap.Push(&pq, &PriorityQueueIntItem{Value: 30, Priority: 50, Index: 3})
-	heap.Push(&pq, &PriorityQueueIntItem{Value: 40, Priority: 30, Index: 4})
+	heap.Push(&pq, 50)
+	heap.Push(&pq, 30)
 
 	results := make([]int, 5)
 	for i := 0; i < 5; i++ {
-		results[i] = heap.Pop(&pq).(*PriorityQueueIntItem).Value
+		results[i] = heap.Pop(&pq).(int)
 	}
 
-	assertEqual(t, []int{30, 10, 40, 0, 20}, results)
-}
-
-func TestUpdate(t *testing.T) {
-	items := make([]*PriorityQueueIntItem, 5)
-	items[0] = &PriorityQueueIntItem{Value: 0, Priority: 20, Index: 0}
-	items[1] = &PriorityQueueIntItem{Value: 10, Priority: 40, Index: 1}
-	items[2] = &PriorityQueueIntItem{Value: 20, Priority: 10, Index: 2}
-	items[3] = &PriorityQueueIntItem{Value: 30, Priority: 50, Index: 3}
-	items[4] = &PriorityQueueIntItem{Value: 40, Priority: 30, Index: 4}
-
-	pq := make(PriorityQueueInt, 5)
-	for i := 0; i < 5; i++ {
-		pq[i] = items[i]
-	}
-
-	heap.Init(&pq)
-
-	pq.Update(items[2], 50, 100)
-
-	results := make([]int, 5)
-	for i := 0; i < 5; i++ {
-		results[i] = heap.Pop(&pq).(*PriorityQueueIntItem).Value
-	}
-
-	assertEqual(t, []int{50, 30, 10, 40, 0}, results)
+	assertEqual(t, []int{10, 20, 30, 40, 50}, results)
 }
 
 func assertEqual(t *testing.T, want, got interface{}) {
